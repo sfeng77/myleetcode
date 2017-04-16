f3b69e1ee252c473279a222c1d2f2bdb96c073ee
DrMushroom <sfeng77@gmail.com>
2017-04-15 23:26:05 -0400

    add weekly contest 28 A-C

---
+++ weeklyContest28.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+
+class Solution():
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        countA = 0
+        countL = 0
+        for c in s:
+            if c == "A":
+                countL = 0
+                countA += 1
+                if countA >= 2:
+                    return False
+            if c == "L":
+                if countL < 2:
+                    countL += 1
+                else:
+                    return False
+            else:
+                countL = 0
+        return True
+
+    def optimalDivision(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        n = len(nums)
+        if n == 0:
+            return ""
+        if n == 1:
+            return str(nums[0])
+        if n == 2:
+            return str(nums[0]) + "/" + str(nums[1])
+        else:
+            return str(nums[0]) + "/(" + "/".join(str(i) for i in nums[1:]) + ")"
+
+    def splitLoopedString(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        n = len(strs)
+        for i in range(n):
+            s = strs[i]
+            if s < s[::-1]:
+                strs[i] = s[::-1]
+
+        js = "".join(strs)
+        maxc = max(js)
+        opts = []
+        for i in range(n):
+            s = strs[i]
+            if maxc not in s:
+                continue
+            ls = len(s)
+            for j in range(ls):
+                if s[j] == maxc:
+                    opt1 = s[j:] + "".join(strs[i+1:]) + "".join(strs[:i]) + s[:j]
+                    opt2 = s[j::-1] + "".join(strs[i+1:]) + "".join(strs[:i]) + s[:j:-1]
+                    opts.append(opt1)
+                    opts.append(opt2)
+        return max(opts)
